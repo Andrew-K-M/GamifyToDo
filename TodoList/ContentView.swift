e92101c8e8230072ba1e45ed05be64c6ea58368f
Andrew-K-M <andrew@mankins.org>
2025-11-24 21:29:54 -0800

    added level up logic

--- TodoList/ContentView.swift
+++ TodoList/ContentView.swift
@@ -10,6 +10,8 @@ import CoreData
 
 struct UserProfile: View {
   @ObservedObject var user: User
+  @Environment(\.managedObjectContext) var viewContext
+  
   var body: some View {
     VStack{
       Text("User Profile")
@@ -20,6 +22,17 @@ struct UserProfile: View {
       Text("Created: \(user.createdAt?.formatted(date:.abbreviated,time:.shortened) ?? "")")
     }
       
+  }
+  private func checkLevelUp(){
+    if user.xp >= 100{
+      user.level += 1
+      user.xp -= 100
+    }
+    do{
+      try viewContext.save()
+    }catch{
+      print("Error saving user level")
+    }
   }
 }
 
@@ -56,99 +69,89 @@ struct ReminderRow: View {
 }
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
+  @Environment(\.managedObjectContext) private var viewContext
     
-    @FetchRequest(
-        entity: Reminder.entity(),
-        sortDescriptors: [NSSortDescriptor(keyPath: \Reminder.title, ascending: true)],
-        animation: .default
-    ) var reminders: FetchedResults<Reminder>
+  @FetchRequest(entity: Reminder.entity(), sortDescriptors: [NSSortDescriptor(keyPath: \Reminder.title, ascending: true)], animation: .default) var reminders: FetchedResults<Reminder>
     
-    @FetchRequest(
-        entity: User.entity(),
-        sortDescriptors: [],
-        //predicate: NSPredicate(format: "name == %@", "andrew"),
-        animation: .default
-    ) var currentUser: FetchedResults<User>
+  @FetchRequest(entity: User.entity(), sortDescriptors: [],animation: .default) var currentUser: FetchedResults<User>
     
-    @State private var isAddReminderDialogPresented = false
+  @State private var isAddReminderDialogPresented = false
     
-    var body: some View {
-      let user = currentUser.first!
-        VStack{
-            TabView{
-                // List Tab
-                VStack{
-                    List{
-                        ForEach(reminders){ reminder in
-                            ReminderRow(reminder: reminder)
-                        }
-                        .onDelete(perform: deleteReminders)
-                    }
-                    .toolbar {
-                        ToolbarItemGroup(placement: .bottomBar){
-                            Spacer()
-                            Button {
-                                isAddReminderDialogPresented.toggle()
-                            } label: {
-                                Image(systemName: "plus")
-                            }
+  var body: some View {
+    let user = currentUser.first!
+      VStack{
+          TabView{
+              // List Tab
+              VStack{
+                  List{
+                      ForEach(reminders){ reminder in
+                          ReminderRow(reminder: reminder)
+                      }
+                      .onDelete(perform: deleteReminders)
+                  }
+                  .toolbar {
+                      ToolbarItemGroup(placement: .bottomBar){
+                          Spacer()
+                          Button {
+                              isAddReminderDialogPresented.toggle()
+                          } label: {
+                              Image(systemName: "plus")
+                          }
                             
-                        }
-                    }
-                    .sheet(isPresented: $isAddReminderDialogPresented) {
-                        ReminderView { title in
-                            guard !title.isEmpty else { return }
-                            addReminder(title: title)
-                            isAddReminderDialogPresented = false
-                        }
-                    }
-                }
-                .tabItem{
-                  Image(systemName: "house.fill")
-                  Text("List")
-                }
-                
-                // USer Tab
-                VStack{
-                  UserProfile(user: user)
-                }
-                .tabItem{
-                    Image(systemName: "person.crop.circle")
-                    Text("User")
-                }
-            }
-            .onAppear {
-              if currentUser.first == nil {
-                _ = loadOrCreateUser(context: viewContext)
+                      }
+                  }
+                  .sheet(isPresented: $isAddReminderDialogPresented) {
+                      ReminderView { title in
+                          guard !title.isEmpty else { return }
+                          addReminder(title: title)
+                          isAddReminderDialogPresented = false
+                      }
+                  }
               }
+              .tabItem{
+                Image(systemName: "house.fill")
+                Text("List")
+              }
+              
+              // USer Tab
+              VStack{
+                UserProfile(user: user)
+              }
+              .tabItem{
+                  Image(systemName: "person.crop.circle")
+                  Text("User")
+              }
+          }
+          .onAppear {
+            if currentUser.first == nil {
+              _ = loadOrCreateUser(context: viewContext)
             }
-        }
+          }
+      }
+  }
+    
+  // Core Data Functions
+  private func addReminder(title: String){
+    let newReminder = Reminder(context: self.viewContext)
+    newReminder.title = title
+    newReminder.createdAt = Date()
+    newReminder.isCompleted = false
+    saveContext()
+  }
+    
+  private func deleteReminders(at offsets: IndexSet){
+    offsets.map { reminders[$0]}.forEach(viewContext.delete)
+      saveContext()
     }
     
-    // Core Data Functions
-    private func addReminder(title: String){
-        let newReminder = Reminder(context: self.viewContext)
-        newReminder.title = title
-        newReminder.createdAt = Date()
-        newReminder.isCompleted = false
-        saveContext()
+  private func saveContext() {
+    do{
+      try viewContext.save()
+    }catch{
+      let nsError = error as NSError
+      print("Error saving context: \(nsError), \(nsError.userInfo)")
     }
-    
-    private func deleteReminders(at offsets: IndexSet){
-        offsets.map { reminders[$0]}.forEach(viewContext.delete)
-        saveContext()
-    }
-    
-    private func saveContext() {
-        do{
-            try viewContext.save()
-        }catch{
-            let nsError = error as NSError
-            print("Error saving context: \(nsError), \(nsError.userInfo)")
-            //fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-        }
-    }
+  }
   
   private func loadOrCreateUser(context: NSManagedObjectContext) -> User{
     let request: NSFetchRequest<User> = User.fetchRequest()
@@ -171,7 +174,7 @@ struct ContentView: View {
 }
 
 #Preview {
-    let context = PersistenceController.shared.container.viewContext
+  let context = PersistenceController.shared.container.viewContext
   ContentView()
         .environment(\.managedObjectContext, context)
         .navigationTitle("Reminders")
